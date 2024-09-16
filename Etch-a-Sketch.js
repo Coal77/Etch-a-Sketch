97d74c67792973933eaad30aaa6965c1308a3237
Coal77 <jomilojuabiola.gmail.com>
2024-09-16 16:35:49 -0700

    first commit

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,16 @@
+let gridContainer = document.getElementById("gridContainer");
+
+// let div1 = document.createElement("div");
+
+let gridSquares;
+let rows = 16;
+let columns = 16;
+let gridSize = 960;
+
+function createGridSquares() {
+    let widthOrHeight = 
+    for(let i = 0; i < (rows * columns); i++) {
+        let gridCells = document.createElement("div");
+        gridCells.style.cssText = "width:"
+    }
+}
